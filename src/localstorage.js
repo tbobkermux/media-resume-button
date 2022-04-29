45db1bdfd43e7296280599396647842f73597840
Dave Kiss <dave@davekiss.com>
2022-04-29 09:17:38 -0400

    Removing unused methods

--- src/localstorage.js
+++ src/localstorage.js
@@ -9,18 +9,8 @@ export const storage = {
       window.localStorage.setItem(JSON.stringify(storage.playbackid), JSON.stringify(data.playhead));
     }
   },
-  clear: function () {
-    window.localStorage.clear();
-  },
   remove: function () {
     window.localStorage.removeItem(JSON.stringify(storage.playbackid));
     console.log(JSON.stringify(storage.playbackid));
-  },
-  isSupported: function () {
-    if (typeof (Storage) !== "undefined") {
-      return true;
-    } else {
-      return false;
-    }
   }
 };
